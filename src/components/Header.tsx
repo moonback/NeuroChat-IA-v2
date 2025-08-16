87206c87b743652203fdb7e59fb78a049f6fe2b0
Medusa <devoye.maysson@gmail.com>
2025-08-16 17:40:22 +0200

    Augmentation de la largeur maximale du conteneur dans le composant Header pour améliorer la mise en page et l'affichage des éléments. Cela permet une meilleure utilisation de l'espace disponible sur les écrans larges.

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -469,7 +469,7 @@ export function Header(props: HeaderProps) {
           <source src="/bip2.ogg" type="audio/ogg" />
         </audio>
 
-        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        <div className="max-w-12xl mx-auto px-4 sm:px-6 lg:px-8">
           <div className="flex items-center justify-between h-16">
             {/* Logo et branding */}
             <div className="flex items-center gap-4">
