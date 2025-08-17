29f8ab20f3e2cc12038077bc707f588bce6c862a
Medusa <devoye.maysson@gmail.com>
2025-08-17 09:49:16 +0200

    Ajout de la prise en charge de la configuration des variables d'environnement dans le serveur Express, en utilisant le chemin du répertoire du fichier pour charger le fichier .env. Cela améliore la gestion des configurations et assure une meilleure portabilité de l'application.

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,12 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
-dotenv.config();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+dotenv.config({ path: path.join(__dirname, '.env') });
 
 const app = express();
 const port = process.env.PORT || 8787;
